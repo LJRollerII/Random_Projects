050d15226ab1757332c9a3e05c6fdfd7a641a262
LJRollerII <ljcoolkid@optonline.net>
2023-08-03 22:57:07 -0400

    Adding code to play games from Rcade library.

---
+++ scripts/Rcade Games/rcade_games.R
@@ -0,0 +1,17 @@
+# install.packages("devtools")
+
+
+Rcade::games
+
+Rcade::games$`2048`
+Rcade::games$Pacman
+Rcade::games$BoulderDash
+Rcade::games$CathTheEgg
+Rcade::games$Core
+Rcade::games$CustomTetris
+Rcade::games$GreenMahjong
+Rcade::games$Mariohtml5
+Rcade::games$Pond
+Rcade::games$SpiderSolitaire
+Rcade::games$SURVIVOR
+
